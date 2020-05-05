0f8933361a2c1e867b6e497af58c389c478a94c3
Hitmasu <flaviodss63@gmail.com>
2020-05-05 01:08:30 -0300

    test build

--- Jitex.Tests/ResolveTokenTests.cs
+++ Jitex.Tests/ResolveTokenTests.cs
@@ -13,8 +13,8 @@ namespace Jitex.Tests
         public ResolveTokenTests()
         {
             ManagedJit jit = ManagedJit.GetInstance();
-            jit.AddCompileResolver(OnResolveCompile);
-            jit.AddTokenResolver(OnResolveToken);
+            //jit.AddCompileResolver(OnResolveCompile);
+            //jit.AddTokenResolver(OnResolveToken);
         }
 
         [Fact]
