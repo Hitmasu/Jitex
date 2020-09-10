16f4c79073158351285eacbf6db64daa169f210c
Hitmasu <flaviodss63@gmail.com>
2020-09-10 20:34:32 -0300

    Added module

---
+++ Jitex/JIT/JitexModule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Jitex.JIT
+{
+    public abstract class JitexModule : IDisposable
+    {
+        private static ManagedJit _jit;
+
+        protected JitexModule()
+        {
+            _jit ??= ManagedJit.GetInstance();
+        }
+
+        public void Initialize()
+        {
+            _jit.AddCompileResolver(CompileResolver);
+            _jit.AddTokenResolver(TokenResolver);
+        }
+
+        protected abstract void CompileResolver(CompileContext context);
+        
+        protected abstract void TokenResolver(TokenContext context);
+
+        protected void RemoveJitex()
+        {
+            _jit.Dispose();
+        }
+        
+        public void Dispose()
+        {
+            _jit.RemoveCompileResolver(CompileResolver);
+            _jit.RemoveTokenResolver(TokenResolver);
+        }
+    }
+}
