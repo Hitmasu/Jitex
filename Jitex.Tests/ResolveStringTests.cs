4442dd1b94de3f981c5ec6730d2a61f9981cbc77
Hitmasu <flaviodss63@gmail.com>
2020-10-04 20:14:12 -0300

    Added string test

---
+++ Jitex.Tests/ResolveStringTests.cs
@@ -0,0 +1,27 @@
+﻿using Jitex.JIT.Context;
+using Xunit;
+
+namespace Jitex.Tests
+{
+    public class ResolveStringTests
+    {
+        static ResolveStringTests()
+        {
+            JitexManager.AddMethodResolver(context => { });
+            JitexManager.AddTokenResolver(TokenResolver);
+        }
+
+        [Fact]
+        public void ResolveString()
+        {
+            string text = "Lorem ipsum dolor";
+            Assert.True(text == "A fox jump over lazy dog", "String not replaced!");
+        }
+
+        private static void TokenResolver(TokenContext context)
+        {
+            if (context.Content == "Lorem ipsum dolor")
+                context.ResolveString("A fox jump over lazy dog");
+        }
+    }
+}
