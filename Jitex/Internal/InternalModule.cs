9637f5fee0c9e65002b0a7dd04b726e277d85ed8
Hitmasu <flaviodss63@gmail.com>
2020-11-23 18:17:59 -0300

    Added InternalModule

---
+++ Jitex/Internal/InternalModule.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Jitex.JIT.Context;
+
+namespace Jitex.Internal
+{
+    internal class InternalModule : JitexModule
+    {
+        private static InternalModule? _instance;
+
+        private InternalModule()
+        {
+            
+        }
+
+        private readonly ConcurrentDictionary<MethodBase, IList<int>> _internalTokens = new ConcurrentDictionary<MethodBase, IList<int>>();
+
+        public static InternalModule GetInstance()
+        {
+            return _instance ??= new InternalModule();
+        }
+
+        protected override void MethodResolver(MethodContext context)
+        {
+        }
+
+        protected override void TokenResolver(TokenContext context)
+        {
+            if (context.Source == null)
+                return;
+
+            if (_internalTokens.TryGetValue(context.Source, out IList<int> tokens))
+            {
+                if (!tokens.Contains(context.MetadataToken))
+                    return;
+
+                tokens.Remove(context.MetadataToken);
+
+                if (!tokens.Any())
+                    _internalTokens.TryRemove(context.Source, out _);
+            }
+        }
+
+        public void AddTokenContext(int token, MethodBase source)
+        {
+            //TODO: Find a better way
+            _internalTokens.AddOrUpdate(source, new List<int> { token }, (key, tokens) =>
+            {
+                tokens.Add(token);
+                return tokens;
+            });
+        }
+    }
+}
---
+++ Jitex/JIT/Context/SelfToken.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+namespace Jitex.JIT.Context
+{
+    internal class SelfToken
+    {
+        public MethodBase Source { get; set; }
+        public int Token { get; set; }
+    }
+}
