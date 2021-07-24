73ee8b94421270ac67b367f22bfd4b34bccdcf0e
Hitmasu <13804176+hitmasu@users.noreply.github.com>
2021-07-24 19:36:21 -0300

    Added R2R tests

---
+++ src/Jitex.Tests/Helpers/ReadyToRunTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using Jitex.Utils;
+using Xunit;
+
+namespace Jitex.Tests.Helpers
+{
+    public class ReadyToRunTests
+    {
+        private static readonly MethodBase GetIlGenerator;
+        private static readonly MethodBase MethodNotReadyToRun;
+
+        static ReadyToRunTests()
+        {
+            GetIlGenerator = typeof(DynamicMethod).GetMethod("GetILGenerator", BindingFlags.Public | BindingFlags.Instance, null, Type.EmptyTypes, null);
+            MethodNotReadyToRun = typeof(ReadyToRunTests).GetMethod(nameof(MethodNotR2R), BindingFlags.Public | BindingFlags.Instance);
+        }
+
+        [Fact]
+        public void DetectMethodIsReadyToRunTest()
+        {
+            bool isReadyToRun = MethodHelper.IsReadyToRun(GetIlGenerator);
+            Assert.True(isReadyToRun);
+        }
+
+        [Fact]
+        public void DetectMethodIsNotReadyToRunTest()
+        {
+            bool isReadyToRun = MethodHelper.IsReadyToRun(MethodNotReadyToRun);
+            Assert.False(isReadyToRun);
+        }
+
+        public void MethodNotR2R() { }
+    }
+}
