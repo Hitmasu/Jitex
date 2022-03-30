b5b8d465fa837bbf53082551928efd32886caa55
hitmasu <13804176+Hitmasu@users.noreply.github.com>
2022-03-30 00:42:56 -0300

    Added NoInlining on ctors

--- src/Jitex.Tests.Context/StaticConstructor.cs
+++ src/Jitex.Tests.Context/StaticConstructor.cs
@@ -1,9 +1,12 @@
-﻿namespace Jitex.Tests.Context;
+﻿using System.Runtime.CompilerServices;
+
+namespace Jitex.Tests.Context;
 
 public class StaticConstructor
 {
     public static int Number { get; set; } 
 
+    [MethodImpl(MethodImplOptions.NoInlining)]
     static StaticConstructor()
     {
         Number = 20;
